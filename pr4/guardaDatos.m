58edc260f246907f72d646ca406d9ed3cf0583ae
Rocío García <35541675+rocgar08@users.noreply.github.com>
2019-06-13 22:28:29 +0200

    Actualizada

--- pr4/guardaDatos.m
+++ pr4/guardaDatos.m
@@ -1,31 +1,22 @@
-load('datosp2.mat')
+load('datos.mat')
+%Guardas los angulos y movimientos
 dangulo=datos.angulo
-%da=datos.angulo'
 dEfector=datos.efector
-dFinal=datos.efector(1:329,1:3)'
-x=dFinal(1,1:329)
-y=dFinal(2,1:329)
-z=dFinal(3,1:329)
-posicionfinal=datos.efector(330,1:3)'
-u=posicionfinal(1)
-v=posicionfinal(2)
-w=posicionfinal(3)
-base=dangulo(1:329,1)'
-hombro=dangulo(1:329,2)'
-codo=dangulo(1:329,3)'
-muneca=dangulo(1:329,4)'
+%Tamaño de la matriz
+n = 361
+%A(m,:) es la m.ª fila de la matriz A
+dEfector(n,:)=[]
+posfin=datos.efector(n,:)
+entrada=[]
+for j= 1:n
+    entrada=[entrada;datos.efector(j,:) posfin];
+end
+entrada= entrada'
+%datos salida
+base = dangulo(:,1)'
+hombro = dangulo(:,2)'
+codo = dangulo(:,3)'
+mun = dangulo(:,3)'
 
-% base=da(1,1:276)
-% hombro=da(2,1:276)
-% codo=da(3,1:276)
-% muneca=da(4,1:276)
-% 
-% d=dEfector'
-% X=d(1,1:276)
-% Y=d(2,1:276)
-% Z=d(3,1:276)
-% 
-% u=d(1, 277)
-% v=d(2, 277)
-% w=d(3, 277)
+
 
